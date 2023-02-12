56443c860cf1c07537617d299562642a479b3c39
Kenzo Escuret <kenzo.escuret@epitech.eu>
2023-02-12 14:33:02 +0100

    all anim

--- src/rpg_anim.c
+++ src/rpg_anim.c
@@ -14,6 +14,7 @@ void brimstone_rpg_art(int x, int y);
 void satan_rpg_art(int x, int y);
 void Bat_art(int x, int y, int decal);
 void jesus_kiss(int x, int y, int decal);
+void heal_anim(int x, int y, int decal);
 void citation_art(int x, int y, int *decal, int *go_right, int cit_index);
 void player_tag(int x, int y, int hp, int mp, char *name);
 
@@ -73,7 +74,7 @@ void citation_attack_anim(int monster_type, int stage)
     }
     while (go_right < 2) {
         refresh();
-        attron(COLOR_PAIR(7));
+        attron(COLOR_PAIR(1));
         citation_art(COLS / 2, LINES / 2, &decal, &go_right, cit_index);
         attron(COLOR_PAIR(7));
         player_rpg_art(COLS / 2, LINES / 2);
@@ -103,8 +104,40 @@ void kiss_attack_anim(int monster_type, int stage)
     clear();
     while (decal < 30) {
         refresh();
-        attron(COLOR_PAIR(7));
+        attron(COLOR_PAIR(3));
         jesus_kiss(COLS / 2, LINES / 2, decal);
+        attron(COLOR_PAIR(7));
+        player_rpg_art(COLS / 2, LINES / 2);
+        if (monster_type == 4) {
+            attron(COLOR_PAIR(stage));
+            satan_rpg_art(COLS / 2, LINES / 2);
+        }
+        if (monster_type == 3) {
+            attron(COLOR_PAIR(1));
+            brimstone_rpg_art(COLS / 2, LINES / 2);
+        }
+        if (monster_type == 2) {
+            attron(COLOR_PAIR(6));
+            pentagram_rpg_art(COLS / 2, LINES / 2);
+        }
+        if (monster_type == 1) {
+            attron(COLOR_PAIR(5));
+            demon_rpg_art(COLS / 2, LINES / 2);
+        }
+        decal++;
+        usleep(50000);
+    }
+    attron(COLOR_PAIR(7));
+}
+
+void prayer_anim(int monster_type, int stage)
+{
+    int decal = 0;
+    clear();
+    while (decal < 40) {
+        refresh();
+        attron(COLOR_PAIR(2));
+        heal_anim(COLS / 2, LINES / 2, decal);
         attron(COLOR_PAIR(7));
         player_rpg_art(COLS / 2, LINES / 2);
         if (monster_type == 4) {
@@ -134,4 +167,5 @@ void anim_attack_rpg(int option, int monster_type, int stage)
     if (option == 1) bat_attack_anim(monster_type, stage);
     if (option == 2) citation_attack_anim(monster_type, stage);
     if (option == 3) kiss_attack_anim(monster_type, stage);
+    if (option == 4) prayer_anim(monster_type, stage);
 }
